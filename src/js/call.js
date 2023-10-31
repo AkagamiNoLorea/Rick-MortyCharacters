73f5b11c9eaf18a57550f06cb1dddf642c41c8d1
AkagamiNoLorea <raiseta92@gmail.com>
2023-10-31 11:17:44 +0100

    added API url

--- src/js/call.js
+++ src/js/call.js
@@ -1,6 +1,7 @@
 const characterList = document.getElementById('character-list');
 
 async function getCharacters() {
+    const apiUrl = 'https://rickandmortyapi.com/api/character';
     const response = await fetch(apiUrl);
     const data = await response.json();
 
